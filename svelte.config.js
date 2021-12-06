0fa06f82eb6006b71908fbf546ab0b7bdc544400
Peter Olson <peter.e.c.olson+github@gmail.com>
2021-12-06 01:28:17 -0600

    add start script

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +9,7 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({ out: 'build' }),
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {
